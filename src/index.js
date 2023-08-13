bdf71faeede2408f691db283201a015963d21b7e
connorq <connorquitt@gmail.com>
2023-08-12 22:45:39 -0500

    it be workin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        const form = document.querySelector('form')
+        task = document.getElementById('new-task-description').value
+        setToDo(task)
+        priority(task)
+        form.reset()
+
+    })
+})
+  
+function setToDo(e){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    p.textContent = `${e} `
+    btn.textContent = 'x'
+    p.appendChild(btn)
+    btn.addEventListener('click', handleRemove)
+    document.querySelector('#tasks').appendChild(p)
+}
+  
+  function handleRemove(e) {
+    e.target.parentNode.remove()
+  }
+
+  function priority(e) {
+      console.log(task)
+  }
